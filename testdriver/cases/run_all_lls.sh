a074948663ae43efff6ecafc1b696500bd2e7ec2
Christian Schreiner <cas@cs.utah.edu>
2015-07-17 02:39:19 -0600

    rearranged script to run the .ll file twice, once for stdout, once for stderr, and thereby filter stderr.  Everything is eventually sent to stdout in a controlled way.

--- testdriver/cases/run_all_lls.sh
+++ testdriver/cases/run_all_lls.sh
@@ -3,42 +3,39 @@
 # exit statuses.  It is hoped this will be more effective than trying
 # to maintain a separate test script for every .ll file.
 
-echo2()
-{
-   echo "$@"
-   echo "$@" >> /dev/stderr
-}
-
 
 for ii in `find ../test/lli_undef_fix -maxdepth 1 -iname \*.ll | sort `; do
-   echo2 '#####################################################################'
-   echo2 found test .ll program: $ii
+   echo '#####################################################################'
+   echo found test .ll program: $ii
 
    failure_expected=false
    grep -qE 'TESTDRIVER-BASED: *failure *expected\b' $ii
    if [ $? -eq 0 ]; then
       failure_expected=true
-      echo2 "  " .ll file says failure expected
+      echo "  " .ll file says failure expected
    else
-      echo2 "  " .ll file says success expected
+      echo "  " .ll file says success expected
    fi
 
-   echo2 " "
+   echo " "
    sync
    
    # run the .ll file
    # we run it twice, once to capture stdout, another time to capture stderr.
    $PROJ_ROOT/admin/bin/llifi $ii 2>> /dev/null
+   echo '\n########\n'
+   echo stderr:
    stdbuf -o 0 -e 0 $PROJ_ROOT/admin/bin/llifi $ii 2>&1 1>> /dev/null | \
-   	 sed 's/addr=0x[0-9a-zA-Z]*/addr=dummy_address/' 1>&2
+   	 sed 's/addr=0x[0-9a-zA-Z]*/addr=dummy_address/' 
 
-   # Note: this didn't work
+   # Note: this didn't work, neither did a dozen+ other attempts.  Sheesh.
    #(stdbuf -o 0 -e 0 $PROJ_ROOT/admin/bin/llifi $ii 2>&3 ) 3>&1 1>&2 | \
    #	 sed 's/addr=0x[0-9a-zA-Z]*/addr=dummy_address/'
    ll_status=$?
    echo -n exit status=\"$ll_status\" " "
 
    # process the results
+   echo '\n########\n'
    if [ "$failure_expected" = "true" ]; then
       echo \(Expected nonzero, i.e. .ll program should end with an error.\) 
       if [ $ll_status -eq 0 ]; then
