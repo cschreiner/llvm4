62178df75eaf964ea588d5204da0b4309b6c213e
Christian Schreiner <cas@cs.utah.edu>
2015-07-17 00:08:14 -0600

    changed to force .ll filenames to be included in the stderr, to help debugging efforts when needed.  Unfortunately, when the test script is run with redirected stdout AND stderr, this information is getting lost before it gets to the stderr file.  To be troubleshot.

--- testdriver/cases/run_all_lls.sh
+++ testdriver/cases/run_all_lls.sh
@@ -3,24 +3,35 @@
 # exit statuses.  It is hoped this will be more effective than trying
 # to maintain a separate test script for every .ll file.
 
+echo2()
+{
+   echo "$@"
+   echo "$@" >> /dev/stderr
+}
+
 
 for ii in `find ../test/lli_undef_fix -maxdepth 1 -iname \*.ll | sort `; do
-   echo '#####################################################################'
-   echo found test .ll program: $ii
-   echo " "
+   echo2 '#####################################################################'
+   echo2 found test .ll program: $ii
 
    failure_expected=false
    grep -qE 'TESTDRIVER-BASED: *failure *expected\b' $ii
    if [ $? -eq 0 ]; then
       failure_expected=true
-      echo "  " .ll file says failure expected
+      echo2 "  " .ll file says failure expected
    else
-      echo "  " .ll file says success expected
+      echo2 "  " .ll file says success expected
    fi
+
+   echo2 " "
+   sync
    
+   # run the .ll file
    $PROJ_ROOT/admin/bin/llifi $ii 
    ll_status=$?
    echo -n exit status=\"$ll_status\" " "
+
+   # process the results
    if [ "$failure_expected" = "true" ]; then
       echo \(Expected nonzero, i.e. .ll program should end with an error.\) 
       if [ $ll_status -eq 0 ]; then
