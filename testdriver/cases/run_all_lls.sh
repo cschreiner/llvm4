16c094fb76e18a11af7859470f92753937965266
Christian Schreiner <cas@cs.utah.edu>
2015-07-17 03:12:09 -0600

    tweaked to correctly check the exit status of both runs of the .ll program, and of the output filter.

--- testdriver/cases/run_all_lls.sh
+++ testdriver/cases/run_all_lls.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Run all of the .ll files under test/lli_undef_fix, and check their
 # exit statuses.  It is hoped this will be more effective than trying
 # to maintain a separate test script for every .ll file.
@@ -26,20 +26,25 @@ for ii in `find ../test/lli_undef_fix -maxdepth 1 -iname \*.ll | sort `; do
    # benchmarks in a predictable way.  Sheesh.
    $PROJ_ROOT/admin/bin/llifi $ii 2>> /dev/null
    ll_status1=$?
-   echo '\n########\n'
-   echo stderr:
-   stdbuf -o 0 -e 0 $PROJ_ROOT/admin/bin/llifi $ii 2>&1 1>> /dev/null | \
+   echo -e '\n########'
+   echo -e stderr: '\n'
+   $PROJ_ROOT/admin/bin/llifi $ii 2>&1 1>> /dev/null | \
    	 sed 's/addr=0x[0-9a-zA-Z]*/addr=dummy_address/' 
-   ll_status2=$?
+   ll_status_array=(${PIPESTATUS[@]})
+   ll_status2=${ll_status_array[0]}
+   pipe_status=${ll_status_array[1]}
 
-   # Note: this didn't work, neither did a dozen+ other attempts.  Sheesh.
+   # Note: this didn't work, neither did a dozen+ other attempts to avoid 
+   # running the .ll program twice.  Sheesh.
    #(stdbuf -o 0 -e 0 $PROJ_ROOT/admin/bin/llifi $ii 2>&3 ) 3>&1 1>&2 | \
    #	 sed 's/addr=0x[0-9a-zA-Z]*/addr=dummy_address/'
-   ll_status=$?
    
 
    # process the results
-   echo '\n########\n'
+   echo -e '\n########'
+   if [ $pipe_status -ne 0 ]; then
+      echo TEST FAILURE: stderr filter failed, exit status=\"$pipe_status\".
+   fi
    if [ $ll_status1 -ne $ll_status2 ]; then
       echo TEST FAILURE: statuses disagree:
       echo "  " ll_status1=\"$ll_status1\", ll_status2=\"$ll_status2\"
@@ -47,12 +52,12 @@ for ii in `find ../test/lli_undef_fix -maxdepth 1 -iname \*.ll | sort `; do
    echo -n exit status=\"$ll_status1\" " "
    if [ "$failure_expected" = "true" ]; then
       echo \(Expected nonzero, i.e. .ll program should end with an error.\) 
-      if [ $ll_status -eq 0 ]; then
+      if [ $ll_status1 -eq 0 ]; then
          echo "TEST FAILURE: unexpected .ll success"
       fi
    else
       echo \(Expected zero, i.e. .ll program should end normally.\)
-      if [ $ll_status -ne 0 ]; then
+      if [ $ll_status1 -ne 0 ]; then
          echo "TEST FAILURE: unexpected .ll failure"
       fi
    fi
