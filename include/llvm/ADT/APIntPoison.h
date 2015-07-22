8d88d211be6191e5ffba31b11c01e9d1d2f0dd6f
Christian Schreiner <cas@cs.utah.edu>
2015-07-21 21:49:34 -0600

    added prototypes for targets of the dispatcher functions

--- include/llvm/ADT/APIntPoison.h
+++ include/llvm/ADT/APIntPoison.h
@@ -30,82 +30,96 @@ namespace llvm {
 //                              APIntPoison Namespace
 namespace APIntPoison {
 
-/* There should be exactly one function here for each LLVM instruction
-   that can generate a poison value.
- */
-// TODO: see if more const designations can be added to parameters.
 
-// TODO: add overloads here as appropriate, for example, one operand
+/* ===========================================================================
+   DISPATCHED FUNCTIONS: There should be a function here for each LLVM
+   instruction that has anything in any sense to do with poison.  A
+   couple instructions, like udiv and sdiv, share an function.
+ */
+// TODO2: see if more const designations can be added to parameters.
+
+// Note: add overloads here as needed, for example, one operand
 // may be an integer (signed or not).
 
-void poisonIfNeeded_add( APInt& dest, APInt& lhs, APInt& rhs, 
-			 bool nsw, bool nuw );
-/* Looks like we need separate checks for ++ vs a full add.
-   Add such a function if needed.
+// ---------------------------------------------------------------------------
+// macro DISPATCH_FUNCTIONS_FOR_SCHEME
+// ---------------------------------------------------------------------------
+// \brief quick way to generate all of the prototypes for a scheme's
+// dispatched functons.
+//
+// \param SCHEME: the name of the scheme to use in expansion.  SCHEME 
+//	values usually need to begin with an underscore.  
+//
+// Example: DISPATCH_FUNCTIONS_FOR_SCHEME(_SchemeEtc)
+
+#define DISPATCH_FUNCTIONS_FOR_SCHEME( SCHEME )				\
+  void poisonIfNeeded_add#SCHEME( APInt& dest, APInt& lhs, APInt& rhs,	\
+				  bool nsw, bool nuw );			\
+  /* Looks like we need separate checks for ++ vs a full add.		\
+     Add such a function if needed.					\
+  */									\
+  void poisonIfNeeded_sub#SCHEME( APInt& dest, APInt& lhs, APInt& rhs,	\
+				  bool nsw, bool nuw );			\
+  void poisonIfNeeded_mul#SCHEME( APInt& dest, APInt& lhs, APInt& rhs,	\
+				  bool nsw, bool nuw );			\
+									\
+  void poisonIfNeeded_div#SCHEME( APInt& dest, APInt& lhs, APInt& rhs,	\
+				  bool exact );				\
+  void poisonIfNeeded_div#SCHEME( APInt& dest, APInt& lhs, APInt& rhs ); \
+  void poisonIfNeeded_rem#SCHEME( APInt& dest, APInt& lhs, APInt& rhs ); \
+									\
+  void poisonIfNeeded_bitAnd#SCHEME( APInt& dest,			\
+				     const APInt& lhs, const APInt& rhs ); \
+  void poisonIfNeeded_bitOr#SCHEME( APInt& dest,			\
+				    const APInt& lhs, const APInt& rhs ); \
+  void poisonIfNeeded_bitXor#SCHEME( APInt& dest,			\
+				     const APInt& lhs, const APInt& rhs ); \
+									\
+  void poisonIfNeeded_shl#SCHEME( APInt& dest, APInt& src,		\
+				  unsigned shiftAmt, bool nsw, bool nuw ); \
+  void poisonIfNeeded_lshr#SCHEME( APInt& dest, APInt& src,		\
+				   unsigned shiftAmt, bool exact );	\
+  void poisonIfNeeded_ashr#SCHEME( APInt& dest, APInt& src,		\
+				   unsigned shiftAmt, bool exact );	\
+									\
+  void poisonIfNeeded_select#SCHEME( APInt& dest,			\
+				     const APInt& src1, const APInt& src2, \
+				     const APInt& src3 );		\
+									\
+  void poisonIfNeeded_trunc#SCHEME( APInt& dest, const APInt& src,	\
+				    const unsigned newBitWidth );	\
+  void poisonIfNeeded_sext#SCHEME( APInt& dest, const APInt& src,	\
+				   const unsigned newBitWidth );	\
+  void poisonIfNeeded_zext#SCHEME( APInt& dest, const APInt& src,	\
+				   const unsigned newBitWidth );	\
+									\
+  void poisonIfNeeded_ptrtoint#SCHEME( APInt& dest, const APInt& src );	\
+  void poisonIfNeeded_inttoptr#SCHEME( APInt& dest, const APInt& src );	\
+  void poisonIfNeeded_bitcast#SCHEME( APInt& dest, const APInt& src );	\
+  void poisonIfNeeded_icmp#SCHEME( APInt& dest,				\
+				   const APInt& lhs, const APInt& rhs ); \
+  //  Note: phi and call are handled by normal poison propogation in APInt \
+  void poisonIfNeeded_br#SCHEME();					\
+									\
+  void poisonIfNeeded_getelementptr#SCHEME( Value& dest,		\
+					    APInt& lhs, APInt& rhs,	\
+					    bool inbounds );
+
+public:
+  DISPATCH_FUNCTIONS_FOR_SCHEME();
+private:
+  DISPATCH_FUNCTIONS_FOR_SCHEME(_SchemeEtc);
+  DISPATCH_FUNCTIONS_FOR_SCHEME(_SchemeNuno);
+
+#undef DISPATCH_FUNCTIONS_FOR_SCHEME
+
+/* ===========================================================================
+   END OF DISPATCHED FUNCTIONS
 */
-void poisonIfNeeded_sub( APInt& dest, APInt& lhs, APInt& rhs, 
-			   bool nsw, bool nuw );
-void poisonIfNeeded_mul( APInt& dest, APInt& lhs, APInt& rhs, 
-			   bool nsw, bool nuw );
 
-void poisonIfNeeded_div( APInt& dest, APInt& lhs, APInt& rhs, bool exact );
-void poisonIfNeeded_div( APInt& dest, APInt& lhs, APInt& rhs );
-void poisonIfNeeded_rem( APInt& dest, APInt& lhs, APInt& rhs );
-
-void poisonIfNeeded_bitAnd( APInt& dest, const APInt& lhs, const APInt& rhs );
-void poisonIfNeeded_bitOr( APInt& dest, const APInt& lhs, const APInt& rhs );
-void poisonIfNeeded_bitXor( APInt& dest, const APInt& lhs, const APInt& rhs );
-
-void poisonIfNeeded_shl( APInt& dest, APInt& src, unsigned shiftAmt, 
-    bool nsw, bool nuw );
-void poisonIfNeeded_lshr( APInt& dest, APInt& src, unsigned shiftAmt, 
-    bool exact );
-void poisonIfNeeded_ashr( APInt& dest, APInt& src, unsigned shiftAmt, 
-    bool exact );
-
-void poisonIfNeeded_select( APInt& dest, 
-    const APInt& src1, const APInt& src2, const APInt& src3 );
-
-void poisonIfNeeded_trunc( APInt& dest, const APInt& src, 
-    const unsigned newBitWidth );
-void poisonIfNeeded_sext( APInt& dest, const APInt& src, 
-    const unsigned newBitWidth );
-void poisonIfNeeded_zext( APInt& dest, const APInt& src, 
-    const unsigned newBitWidth );
-
-
-// CAS TODO3: check the types for these arguments.
-void poisonIfNeeded_getelementptr( 
-    Value& dest, APInt& lhs, APInt& rhs, bool inbounds );
-
+public:
 void printIfPoison( Instruction& In, APInt& val );
-// TODO: see if const qualifiers can be applied to I or val
-
-// ----------------------------------------------------------------------------
-///  \fn poisonIfNeeded_div()
-// ----------------------------------------------------------------------------
-/*** \brief propogates poison for a division instruction (udiv or
- * sdiv).  Effectively, the instruction's "exact" flag is assumed to
- * be not present, so no new poison can be created.
- *
- * \b Detailed_Description: 
- *
- * \b Method: 
- *
- * \b Reentrancy: 
- *
- * \param  dest (output) the quotient to check
- * \param  lhs, rhs (inputs) the two operands to check
- *    
- * \return void
- *
- */
-inline void poisonIfNeeded_div( APInt& dest, APInt& lhs, APInt& rhs )  
-{{
-  dest.setPoisoned( lhs.getPoisoned() || rhs.getPoisoned() );
-  return;
-}}
-
+// TODO2: see if const qualifiers can be applied to I or val
 
 } // End of APIntPoison namespace
 
