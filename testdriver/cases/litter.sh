c5c1e7dcfc75d70b8a6da71ae62686a5498c1a9d
Christian Schreiner <cas@cs.utah.edu>
2015-07-23 00:34:04 -0600

    changed output format of the post-lit filter, to make it easier to tell if a .ll file's status changed between PASS and FAIL.  Fixed overlooked :'s in the previous attempt to do this.

--- testdriver/cases/litter.sh
+++ testdriver/cases/litter.sh
@@ -3,10 +3,16 @@
 # be run by another test case.
 
 cd ..
-# We need to change suffixes of form "(574 of 15638)" into something
-# repeatable, as lit does not run the tests in a consistent order.
+# Note: We need to change suffixes of form "(574 of 15638)" into something
+# 	repeatable, as lit does not run the tests in a consistent order.
+# Note: sort -k determines which fields on the line are the sort key.  We
+# 	don't sort by field 1 because it contains the words 'PASS' or 'FAIL', 
+# 	we want to sort by the filename information coming later.  This makes 
+#	it easy to tell if a test difference is because a .ll file that 
+#	used to pass failed, or visa versa, as opposed to a new .ll file 
+#	was added somehow.
 ../build/bin/llvm-lit test | grep -e ^PASS -e ^FAIL -e ^X -e ^UNSUPPORTED | \
-      sed 's/[0-9][0-9]* of [0-9][0-9]*/xx of yy/' | sort 
+      sed 's/[0-9][0-9]* of [0-9][0-9]*/xx of yy/' | sort -k 2,9 
 lit_status=$?
 
 echo lit exit status=\"$lit_status\"
