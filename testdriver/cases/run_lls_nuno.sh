8e24d82e6d913a33ca44a3001dae843ed4e2f540
Christian Schreiner <cas@cs.utah.edu>
2015-08-31 16:45:13 -0600

    added no_exit_due_to_poison flag, which hopefully will allow us to execute more than one poison-generating opcode per .ll file

--- testdriver/cases/run_lls_nuno.sh
+++ testdriver/cases/run_lls_nuno.sh
@@ -5,6 +5,6 @@
 # than trying # to maintain a separate test script for every .ll file.
 
 
-export LLI_LUF_OPTS=nuno
+export LLI_LUF_OPTS=nuno,no_exit_due_to_poison
 source scripts/run_lli_undef_fix_lls.sh
 
